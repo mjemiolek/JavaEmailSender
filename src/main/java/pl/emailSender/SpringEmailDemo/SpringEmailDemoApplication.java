0521c0d1bbd3a06f4d42e14b683b1cdbc5cb63e2
Michał Jemiołek <81627280+mjemiolek@users.noreply.github.com>
2022-05-30 19:15:57 +0200

    EmailSenderService

--- src/main/java/pl/emailSender/SpringEmailDemo/SpringEmailDemoApplication.java
+++ src/main/java/pl/emailSender/SpringEmailDemo/SpringEmailDemoApplication.java
@@ -1,13 +1,25 @@
 package pl.emailSender.SpringEmailDemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SpringEmailDemoApplication {
+	@Autowired
+	private EmailSenderService senderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmailDemoApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMail()
+	{
+		senderService.sendMail("michal.jemiolek@gmail.com","Email from java bank system",
+				"Hello user.\nWe are sorry but you don't have creditworthiness to take a loan in our bank");
 	}
 
 }
