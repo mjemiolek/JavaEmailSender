0dab765d3956f50f0a994ad8b5e8449abb0b2579
Michał Jemiołek <81627280+mjemiolek@users.noreply.github.com>
2022-05-30 15:11:06 +0200

    spring initialization

---
+++ src/main/java/pl/emailSender/SpringEmailDemo/SpringEmailDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.emailSender.SpringEmailDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmailDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/emailSender/SpringEmailDemo/SpringEmailDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.emailSender.SpringEmailDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmailDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
